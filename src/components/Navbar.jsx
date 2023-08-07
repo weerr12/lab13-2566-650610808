662cd500ddb790b5741e9c9a04a11b2226d586a7
Chayanin <arm.crazyplay@gmail.com>
2023-08-07 11:20:28 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center gap-5 fs-4">
+        <Link href="/">Home</Link>
+        <Link href="/movie">Movie</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+      <hr />
+    </div>
+  );
+};
