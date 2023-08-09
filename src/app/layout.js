90059ad1f6f7114ba3c0ff4a3c17f3c4a313baef
weerr12 <jokerwwe0542@gmail.com>
2023-08-09 12:52:26 +0700

    ko

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Phurinon Foeythika"
+            studentId="650610800"
           />
         </div>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
-"use client";
+import { MovieRow } from "@/components/MovieRow";
+import { movieDB } from "@/libs/movieDB";
 
-export default function HomePage() {
+export default function MoviePage() {
   return (
     <div>
-      <div className="text-center">
-        <p className="fw-bold fs-4">Welcome to TOP 10 IMDB</p>
-        <p>We show top 10 movies of all time!</p>
-      </div>
+      <p className="fw-bold fs-4 text-center">Top 10 Movies</p>
+      {movieDB.map((movie, i) => (
+        <MovieRow
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          detail={movie.detail}
+          rating={movie.rating}
+          number={i + 1}
+        />
+      ))}
     </div>
   );
 }
